89ce335c68134ceeb0ce3bade03a2fc64dcaa57b
Vadim Fominykh <vadim.fominykh91@gmail.com>
2018-12-02 23:00:21 +0700

    Third screen

--- src/components/page3.vue
+++ src/components/page3.vue
@@ -1,3 +1,50 @@
 <template>
-  <h2>Third page</h2>
+  <div>
+    <h2 class="title is-5">Дополнительная информация</h2>
+    <form @submit.prevent="validate">
+      <label class="label">Любимый цвет</label>
+      <div class="buttons">
+        <button v-for="(color, index) of colors"
+              class="button colored"
+              type="button"
+              :style="{ 'background-color': color }"
+              @click="setFavoriteColor(color)"
+              :key="index" />
+      </div>
+
+      <div class="field">
+        <div class="control">
+          <button class="button is-link">продолжить</button>
+        </div>
+      </div>
+    </form>
+  </div>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        favoriteColor: undefined,
+        colors: ['white', 'black', 'red', 'green', 'yellow', 'blue', 'pink', 'orange', 'gray']
+      }
+    },
+    methods: {
+      validate () {
+        this.$router.push('fourth')
+      },
+      setFavoriteColor (color) {
+        this.favoriteColor = color;
+      },
+    },
+  }
+</script>
+
+<style lang="scss">
+  .colored {
+    width: 36px;
+  }
+  .buttons {
+    width: 140px;
+  }
+</style>
