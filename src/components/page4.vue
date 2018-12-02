49a542db7603a8ebc2567a47177719ab52ea0c59
Vadim Fominykh <vadim.fominykh91@gmail.com>
2018-12-03 00:48:04 +0700

    Fourth screen

--- src/components/page4.vue
+++ src/components/page4.vue
@@ -1,3 +1,27 @@
 <template>
-  <h2>Forth page</h2>
+  <div>
+    <h2 class="title is-5 has-text-centered">Благодарим за ответы!</h2>
+    <div class="test">
+      <button class="button is-link" @click="close">пожалуйста</button>
+    </div>
+  </div>
 </template>
+
+<script>
+  export default {
+    methods: {
+      close () {
+        alert('Здесь окно должно было закрыться, но я не нашел способа сделать это' +
+          ' в виду того, что сейчас скрипт может закрыть только то окно, которое сам же и открыл.');
+      },
+    },
+  }
+</script>
+
+<style lang="scss">
+  .test {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
